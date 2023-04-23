b1ae684ec674b8e977704b45072d55623a834c58
MA <60564425+GTI-Dev@users.noreply.github.com>
2023-04-23 23:53:12 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-cd /app
-echo "Starting Deployment" 
-gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
+#!/bin/sh
+
+# Set environment variables
+export BOT_TOKEN=6244396357:AAHs-wyMlu7R7SZ2gFP9jTAmUoCQ02Byo24
+
+# Start the bot
+python app.py
